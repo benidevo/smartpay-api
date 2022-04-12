10794405b9a8f017c7bf93a109717ab5d403654c
Benjamin Idewor <benjaminidewor@gmail.com>
2022-04-12 16:09:15 +0100

    feat: Add delete and update items methods

--- src/controllers/items/index.js
+++ src/controllers/items/index.js
@@ -1,7 +1,11 @@
 const { getAllItems } = require("./getAllItems");
 const createItem = require("./createItem");
+const updateItem = require("./updateItem");
+const deleteItem = require("./deleteItem");
 
 module.exports = {
   showAll: getAllItems,
   create: createItem,
+  update: updateItem,
+  delete: deleteItem,
 };
--- src/routes/items.routes.js
+++ src/routes/items.routes.js
@@ -7,4 +7,8 @@ router.get("/", ItemsController.showAll);
 
 router.post("/", ItemsController.create);
 
+router.patch("/:id", ItemsController.update);
+
+router.delete("/:id", ItemsController.delete);
+
 module.exports = router;
