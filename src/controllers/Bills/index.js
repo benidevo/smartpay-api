4f07a4d091dfd427fe98729971c4b7862bfa55c4
Benjamin Idewor <benjaminidewor@gmail.com>
2022-04-15 12:03:45 +0100

    Feat: add get-bills endpoint

--- src/controllers/Bills/index.js
+++ src/controllers/Bills/index.js
@@ -1,5 +1,7 @@
 const createBill = require("./createBill");
+const getAllBills = require("./getAllBills");
 
 module.exports = {
   create: createBill,
+  list: getAllBills,
 };
--- src/routes/bills.routes.js
+++ src/routes/bills.routes.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.post("/", auth, BillsController.create);
 
+router.get("/", auth, BillsController.list);
+
 module.exports = router;
